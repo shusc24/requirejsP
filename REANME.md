4cd8f664e8d7cdc613ae01723773ff6bbe418a79
shusc24 <shusc234@hotmail.com>
2017-12-17 20:05:03 +0800

    Initial commit

---
+++ REANME.md
@@ -0,0 +1,35 @@
+#requrieP
+##该插件是讲requirejs中的require转换成类似promise语法
+  使用在webpack打包下进行异步加载  
+
+## 使用
+````
+    //通用
+    requireP(['jquery:$','Vue']).then(function(lib){
+        var $ = lib.$;
+        var Vue = lib.Vue;
+    })
+
+   //AMD
+   require(['requireP'],function(requireP){
+         let testComponet = async function (resolve) {
+             resolve(await requireP('test'));
+         };
+   })  
+   
+   //commonJs
+   const requireP = require('requireP');
+   
+   async function test (){
+        let Vue = await requireP('Vue') 
+   }
+   
+   test()
+   
+   //使用数组别名
+   async function test2(){
+        let {Vue,$} = await requireP(['Vue','jquery:$'])
+   }
+   
+   test2()
+````
---
+++ requirejs-promise.ts
@@ -0,0 +1,62 @@
+declare let requirejs:any,Promise:any,define:any,exports:any,module:any;
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define("requireP",[], factory);
+    } else if (typeof exports === 'object') {
+        // Node, CommonJS之类的
+        module.exports = factory();
+    } else {
+        // 浏览器全局变量(root 即 window)
+        root.requireP = factory();
+    }
+}(this, function () {
+    const requireP= function (deps) {
+        class AmdPromise {
+            private DepsArr:any;
+            private aliasArr:any;
+            constructor() {
+                this.DepsArr = [];
+                this.aliasArr = [];
+                if (Array.isArray(deps)) {
+                    deps.forEach(dep => {
+                        let depString = dep.split(":");
+                        let depName = depString[0];
+                        let aliasName = depString[1];
+                        this.aliasArr.push(aliasName || depName);
+                        this.DepsArr.push(this.$createAmd(depName));
+                    });
+                } else {
+                    this.DepsArr.push(this.$createAmd(deps))
+                }
+            }
+            then(fn) {
+                Promise.all((() => {
+                    return this.DepsArr.map(depFn => {
+                        return depFn()
+                    })
+                })()).then(libs => {
+                    if(Array.isArray(deps)){
+                        let Obj = {};
+                        deps.forEach((dep, index) => {
+                            Obj[this.aliasArr[index]] = libs[index];
+                        });
+                        fn.call(null, Obj)
+                    }else{
+                        fn.apply(null, libs)
+                    }
+                });
+                return this
+            }
+            $createAmd(lib) {
+                return function () {
+                    return new Promise(resolve => {
+                        requirejs([lib], resolve)
+                    })
+                }
+            }
+        }
+        return new AmdPromise()
+    };
+    return requireP;
+}));
